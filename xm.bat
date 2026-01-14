0196b348d7d9a4ca19677600b4f7cc01faf3d743
Thabiso Nthako <thabiso.nthako@gmail.com>
2026-01-14 18:59:29 +0200

    Update xm.bat

--- xm.bat
+++ xm.bat
@@ -1,2 +1,2 @@
-xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 4G2VHCK4o1PRcKnLApZuPERbMBgHsCfHWeQFY1D4kPgxQnM9bUbKDoRV9HiUXMKDzQhnG4YfkN57RiZa7LvRCJbzGmMjXmpYwkuKaAHfXD.rigName -p x
+xmrig.exe --coin=XMR -o ca.monero.herominers.com:1111 -u 4G2VHCK4o1PRcKnLApZuPERbMBgHsCfHWeQFY1D4kPgxQnM9bUbKDoRV9HiUXMKDzQhnG4YfkN57RiZa7LvRCJbzGmMjXmpYwkuKaAHfXD.rigName -p x
 pause
